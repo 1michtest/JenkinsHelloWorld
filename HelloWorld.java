53dd4dfc368247acb29972bd9391b5d6e2d164c8
1michtest <55572214+1michtest@users.noreply.github.com>
2020-04-15 00:23:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
     class HelloWorld{  
         public static void main(String args[]){  
-         System.out.println("Hello, This is simple Java programe!! Hello World");  
+         System.out.println("Hello, This is simple Java programe!! Hello World !! Edited in GitHUB");  
         }  
     }  
